64bc893e852ee75e84cbad5d6095ed7150ddce5f
yoosh2842 <thereqltimel@gmail.com>
2023-02-19 01:11:52 -0800

    fixed map

--- app/services/MapImage.py
+++ app/services/MapImage.py
@@ -24,9 +24,10 @@ def mapImage(notes):
         
         return root, graph
     #data should be a response from openai GPT3
-        import openai
+    import openai
     max_tokens = 2000
-    question = "Create an outputs similar to this set: ['pets', ['dog', 'cat'], [['bark', 'walk'], ['meow', 'purr']], [[['noise', 'dog noise'], ['action', 'action again']], [['noise', 'cat noise'], ['action', 'cat action']]]] \n\n where the first word is byitself/the root, noise and dog noise relate to bark, action and action again relate to walk, noise and cat noise relate to meow, and action and cat action relate to purr\n\nso that each successive layer has an extra set of brackets using this text instead (make sure to extract keywords only and only create the output based on similarity/correlation between the keywords and key reminder is that you should create 2 different sets so that you don't have to relate stuff that isn't related to each other -> do this in the form of Set1:  \n Set2: ): \n\n" + notes
+    
+    question = "Create an outputs similar to this set as a json: {'Pets': { 'Dog': { 'Bark': {'noise': {}, 'dog noise': {} }, 'Walk': {'action' : {}, 'action again' : {}}  }, 'Cat': {'Meow': { 'noise': {}, 'cat noise': {} },'Purr': { 'action': {}, 'cat action': {} }  } }} \n\n where the first word is byitself/the root, noise and dog noise relate to bark, action and action again relate to walk, noise and cat noise relate to meow, and action and cat action relate to purr\n\nso that each successive layer has an extra set of curly braces using this text instead (make sure to extract keywords only and only create the output based on similarity/correlation between the keywords and key reminder is that you should create 2 different sets so that you don't have to relate stuff that isn't related to each other -> do this in the form of Set1:  \n Set2: ): \n\n" + notes
     response = openai.Completion.create(
                         engine="text-davinci-003",
                         prompt=question,
@@ -41,9 +42,10 @@ def mapImage(notes):
     #data = list(response["choices"][0]["text"].strip())
     import ast
     data = response.split(":")[1][1:-5].strip()
-    data2 = response.split(":")[-1][1:].strip()
-    data = ast.literal_eval(data)
-    data2 = ast.literal_eval(data2)
+    #data2 = response.split(":")[-1][1:].strip()
+
+    #data = ast.literal_eval(data)
+    #ata2 = ast.literal_eval(data2)
 
 
 
@@ -57,14 +59,34 @@ def mapImage(notes):
                 result.append(item)
         return result
 
-    data = flatten(data)
-    data2 = flatten(data2)
-    root1, graph1 = create_binary_tree(data)
-    root2, graph2 = create_binary_tree(data2)
+    #data = flatten(data)
+    #data2 = flatten(data2)
+    #root1, graph1 = create_binary_tree(data)
+    #root2, graph2 = create_binary_tree(data2)
     #graph1.add_edge(pydot.Edge(root1, root2, dir='none'))
     #nx.draw(graph1)
-
+    from graphviz import Digraph#
+    # Define a function to build the graph
+    while True:
+        try:
+            tree = data
+            def build_graph(graph, node, parent=None):    
+                for k, v in node.items():        
+                    if parent:            
+                        graph.edge(parent, k)        
+                        graph.node(k)        
+                    if v:            
+                        build_graph(graph, v, k)
+                        # Create a new Digraph object
+            dot = Digraph()# Build the graph
+            build_graph(dot, tree)# Output the graph as a PNG file
+           
+            break
+        except:
+            data += "}"
+    
+    #dot.render('tree', format='png')
     #download images on frontend
-    graph1.write_png(f'mindmaps/set1_tree{notes[:10]}.png')
+    dot.render(f'mindmaps/set1_tree{notes[:10]}.png')
     return f'set1_tree{notes[:10]}.png'
     graph2.write_png('mindmaps/set2_tree.png')
