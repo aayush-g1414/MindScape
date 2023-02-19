a16e759cdb3b3e8ed77cbc8476a35ca333b4bbe7
GoZaddy <faruqyusuff437@gmail.com>
2023-02-19 08:17:40 -0800

    even more updates

--- app/services/MapImage.py
+++ app/services/MapImage.py
@@ -56,8 +56,8 @@ def mapImage(notes):
     from graphviz import Digraph#
     import json
     # Define a function to build the graph
-    print(type(data))
-    print(data)
+    # print(type(data))
+    # print(data)
     while True:
         try:
             question = "Create an outputs similar to this set as a json: {'Pets': { 'Dog': { 'Bark': {'noise': { }, 'dog noise': { } }, 'Walk': {'action' : { }, 'action again' : { } }  }, 'Cat': {'Meow': { 'noise': { }, 'cat noise': { } },'Purr': { 'action': { }, 'cat action': { } }  } } } \n\n where the first word is byitself/the root, noise and dog noise relate to bark, action and action again relate to walk, noise and cat noise relate to meow, and action and cat action relate to purr\n\nso that each successive layer has an extra set of curly braces using this text instead (make sure to extract keywords only and only create the output based on similarity/correlation between the keywords and key reminder is that you should create 2 different sets so that you don't have to relate stuff that isn't related to each other and keep each layer between 2^n and 4^n elements-> do this in the form of Set1:  \n Set2: ): \n\n" + notes
